928ee5f21fe72ba81d760f276929928c52eea22e
Cristian Rodriguez <cristian_rocas@hotmail.com>
2016-11-13 18:55:13 -0800

    added couple of functions to javascript.js

--- javascript.js
+++ javascript.js
@@ -1,66 +1,112 @@
-// var config = {
-//     apiKey: "AIzaSyAn00ikrENl5il1EdZOUueJQLY_CqLPhWQ",
-//     authDomain: "dinner-on-me.firebaseapp.com",
-//     databaseURL: "https://dinner-on-me.firebaseio.com",
-//     storageBucket: "dinner-on-me.appspot.com",
-//     messagingSenderId: "310258061148"
-//   };
-//   firebase.initializeApp(config);
-
-//   var database = firebase.database();
-
-//   var key = AIzaSyCnaUowCn8tSao1lV56ztYhaIKG_TdH2TU
-  
- var meals = ['Breakfast','Lunch','Dinner','Dessert'];
-
-var Breakfast = {
-	options:['Pancakes','Eggs','Omelet'],
-	price: ['$','$$','$$$']
+// These are all the opjects of meals, and their options for types of foods 
+var breakfast = {
+	meal: 'Breakfast',
+	options: ['Pancakes','Eggs','Omelet'],
+	index: 0
 };
-var Lunch = {
+var lunch = {
+	meal: 'Lunch',
 	options:['Mexican','Indian','Greek','Vietnamese','Chinese','Sushi'],
-	price: ['$','$$','$$$']
+	index: 1
 };
-var Dinner = {
+var dinner = {
+	meal: 'Dinner',
 	options:['Mexican','Indian','Greek','Vietnamese','Chinese','Sushi'],
-	price: ['$','$$','$$$']
+	index: 2
 };
-var Dessert = {
-	optiosn:['Cheesecake','Pie','Flan'],
-	price:['$','$$','$$$']
+var dessert = {
+	meal:'Dessert',
+	options:['Cheesecake','Pie','Flan'],
+	index:3
 }
 
+// Array containing the objects above
+var meals = [breakfast,lunch,dinner,dessert];
+
+// Array for meal price
+var price = ['$','$$','$$$'];
+
+//Empty var to store the meal chosen
 var mealChosen = '';
 
-function buttonsViewEmpty () {
-	$('#buttonsView').empty();
-}
+// App starts 
+$(document).ready(function(){
 
+	// Click function for meal buttons
 	$('#submit').on('click',function(){
-		$('#buttonsView').empty();
+
+		// Delete the buttons from HTML
+		emptyButtonsView();
+
+		// Create the buttons for the meal options
 		mealButtons();
+
+		// Click function to pick meals (breakfast,lunch,dinner,dessert)
 		$('.meal').on('click',function(){
+
+			// Store meal chosen to global variable
 			mealChosen = $(this).data('meal');
 
-			if (mealChosen === 'Breakfast') {
-				console.log('I Chose breakfast');
+			emptyButtonsView();
 
-			} else if (mealChosen === 'Lunch') {
-				console.log ('I Chose Lunch')
-			} else if (mealChosen === 'Dinner') {
-				console.log('I chose Dinner');
-			} else {
-				console.log('I want some dessrt');
-			}
-		});
-	})
+			// Grabs the index number inside the meals array
+			var indexMeals = $(this).data('index');
 
+			optionsButtons(indexMeals);
+
+			// Click function for meal 
+			$('.options').on('click',function() {
+				console.log('this works');
+
+
+			})
+
+		}); // meal options on click end
+
+	}) // End Submit on click 
+
+}); // App ends
+
+
+	// Deletes everything on $('#buttonsView') in the HTML 
+	function emptyButtonsView () {
+		$('#buttonsView').empty();
+	}
+
+	// Functons that creates the meal buttons
 	function mealButtons() {
 		for (var i = 0; i < meals.length; i++) {
 			var $mealButtons = $('<button>');
-			$mealButtons.text(meals[i]);
-			$mealButtons.attr('class','meal btn btn-success')
-			$mealButtons.attr('data-meal',meals[i]);
+			$mealButtons.text(meals[i].meal);
+			$mealButtons.attr('class','meal btn btn-success');
+			$mealButtons.attr('data-meal',meals[i].meal);
+			$mealButtons.attr('data-index',meals[i].index);
+			$mealButtons.attr('options',meals[i].options);
 			$mealButtons.appendTo('#buttonsView');
 		}
 	}
+
+	// Funstion that creates the meal options buttons
+	function optionsButtons(indexMeals) {
+		var indexOptions =meals[indexMeals].options
+		for (var j = 0; j < indexOptions.length; j++) {
+			var $optionsButtons = $('<button>');
+			$optionsButtons.text(indexOptions[j]);
+			$optionsButtons.attr('class','options btn btn-danger');
+			$optionsButtons.attr('data-mealOption', indexOptions[j]);
+			$optionsButtons.appendTo('#buttonsView');
+			console.log($optionsButtons);
+		}
+	}
+
+	// Function that creates the price buttons
+	function priceButtons () {
+		for (var money=0; money<price.length; money++) {
+			var $moneyButtons = $('<buttons>');
+			$moneyButtons.text(price[money]);
+			$moneyButtons.attr('class', 'price btn btn-primary');
+			$moneyButtons.attr('data-price',price[money]);
+			$moneyButtons.appentTo('#buttonsView');
+		}
+	}
+
