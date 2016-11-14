3c2e1953faa21270705f830436ca62878fa03034
Cristian Rodriguez <cristian_rocas@hotmail.com>
2016-11-14 10:59:31 -0800

    Working on the creation of the buttons after clicking and storing users choioces

--- javascript.js
+++ javascript.js
@@ -26,8 +26,10 @@ var meals = [breakfast,lunch,dinner,dessert];
 // Array for meal price
 var price = ['$','$$','$$$'];
 
-//Empty var to store the meal chosen
+//Empty var to store users preferences
 var mealChosen = '';
+var mealType = '';
+var price = '';
 
 // App starts 
 $(document).ready(function(){
@@ -35,38 +37,12 @@ $(document).ready(function(){
 	// Click function for meal buttons
 	$('#submit').on('click',function(){
 
-		// Delete the buttons from HTML
-		emptyButtonsView();
-
 		// Create the buttons for the meal options
 		mealButtons();
 
-		// Click function to pick meals (breakfast,lunch,dinner,dessert)
-		$('.meal').on('click',function(){
-
-			// Store meal chosen to global variable
-			mealChosen = $(this).data('meal');
-
-			emptyButtonsView();
-
-			// Grabs the index number inside the meals array
-			var indexMeals = $(this).data('index');
-
-			optionsButtons(indexMeals);
-
-			// Click function for meal 
-			$('.options').on('click',function() {
-				console.log('this works');
-
-
-			})
-
-		}); // meal options on click end
-
-	}) // End Submit on click 
+	}); // End Submit on click 
 
 }); // App ends
-
 
 	// Deletes everything on $('#buttonsView') in the HTML 
 	function emptyButtonsView () {
@@ -75,6 +51,9 @@ $(document).ready(function(){
 
 	// Functons that creates the meal buttons
 	function mealButtons() {
+		emptyButtonsView();
+
+		// 
 		for (var i = 0; i < meals.length; i++) {
 			var $mealButtons = $('<button>');
 			$mealButtons.text(meals[i].meal);
@@ -84,29 +63,54 @@ $(document).ready(function(){
 			$mealButtons.attr('options',meals[i].options);
 			$mealButtons.appendTo('#buttonsView');
 		}
+		// Click function to pick meals (breakfast,lunch,dinner,dessert)
+		$('.meal').on('click',function(){
+
+			// Store meal chosen to global variable
+			mealChosen = $(this).data('meal');
+
+			// Grabs the index number inside the meals array
+			var indexMeals = $(this).data('index');
+
+			optionsButtons(indexMeals);
+
+		});
 	}
 
 	// Funstion that creates the meal options buttons
 	function optionsButtons(indexMeals) {
+		emptyButtonsView();
 		var indexOptions =meals[indexMeals].options
 		for (var j = 0; j < indexOptions.length; j++) {
 			var $optionsButtons = $('<button>');
 			$optionsButtons.text(indexOptions[j]);
 			$optionsButtons.attr('class','options btn btn-danger');
-			$optionsButtons.attr('data-mealOption', indexOptions[j]);
+			$optionsButtons.data('mealOption', indexOptions[j]);
 			$optionsButtons.appendTo('#buttonsView');
-			console.log($optionsButtons);
 		}
+		// Click function for meal 
+		$('.options').on('click',function() {
+			mealType = ($(this).data('mealOption'));
+			priceButtons();
+
+		});
 	}
 
 	// Function that creates the price buttons
 	function priceButtons () {
+		emptyButtonsView();
 		for (var money=0; money<price.length; money++) {
 			var $moneyButtons = $('<buttons>');
 			$moneyButtons.text(price[money]);
 			$moneyButtons.attr('class', 'price btn btn-primary');
-			$moneyButtons.attr('data-price',price[money]);
-			$moneyButtons.appentTo('#buttonsView');
+			$moneyButtons.data('price',price[money]);
+			$moneyButtons.appendTo('#buttonsView');
 		}
+
+		$('.price').on('click',function(){
+			price = $(this).data('price');
+		})
 	}
+
+
 
